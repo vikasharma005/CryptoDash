320f77a163db237fa765d265c7cf75f96fc3f75f
Vikas Sharma <93978146+vikasharma005@users.noreply.github.com>
2023-08-21 21:48:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,8 @@ from urllib.request import urlopen
 
 # Titles and subtitles
 st.title("Cryptocurrency Daily Prices | ₿")
-st.header("Main Dashboard")
-st.subheader("you can add more crypto in code </>")
-
+#st.header("Main Dashboard")
+#st.subheader("You can add more crypto in code </>")
 
 # Defining ticker variables
 Bitcoin = 'BTC-USD'
@@ -21,61 +20,42 @@ Ethereum = 'ETH-USD'
 Ripple = 'XRP-USD'
 BitcoinCash = "BCH-USD"
 
+# User input for date range
+start_date = st.date_input("Select a start date", value=(2021, 11, 19))
+end_date = st.date_input("Select an end date", value=(2021, 11, 19))
+
 # Access data from Yahoo Finance
 BTC_Data = yf.Ticker(Bitcoin)
 ETH_Data = yf.Ticker(Ethereum)
 XRP_Data = yf.Ticker(Ripple)
 BCH_Data = yf.Ticker(BitcoinCash)
 
+# Fetch history data from Yahoo Finance based on user input
+BTCHis = BTC_Data.history(period="max", start=start_date, end=end_date)
+ETHHis = ETH_Data.history(period="max", start=start_date, end=end_date)
+XRPHis = XRP_Data.history(period="max", start=start_date, end=end_date)
+BCHHis = BCH_Data.history(period="max", start=start_date, end=end_date)
 
-#Fetch history data from Yahoo Finance
-BTCHis = BTC_Data.history(period="max")
-ETHHis = ETH_Data.history(period="max")
-XRPHis = XRP_Data.history(period="max")
-BCHHis = BCH_Data.history(period="max")
+# Fetch crypto data for the dataframe based on user input
+BTC = yf.download(Bitcoin, start=start_date, end=end_date)
+ETH = yf.download(Ethereum, start=start_date, end=end_date)
+XRP = yf.download(Ripple, start=start_date, end=end_date)
+BCH = yf.download(BitcoinCash, start=start_date, end=end_date)
 
-# Fetch crypto data for the dataframe
-BTC = yf.download(Bitcoin, start="2021-11-19", end="2021-11-19")
-ETH = yf.download(Ethereum, start="2021-11-19", end="2021-11-19")
-XRP = yf.download(Ripple, start="2021-11-19", end="2021-11-19")
-BCH = yf.download(BitcoinCash, start="2021-11-19", end="2021-11-19")
+# Display data for each cryptocurrency
+cryptos = [
+    ("BITCOIN", imageBTC, BTC, BTCHis),
+    ("ETHERUM", imageETH, ETH, ETHHis),
+    ("RIPPLE", imageXRP, XRP, XRPHis),
+    ("BITCOIN CASH", imageBCH, BCH, BCHHis)
+]
 
-#Bitcoin
-st.write("BITCOIN ($)")
-imageBTC = Image.open(urlopen('https://s2.coinmarketcap.com/static/img/coins/64x64/1.png'))
-#Display image
-st.image(imageBTC)
-#Display dataframe
-st.table(BTC)
-#Display a chart
-st.bar_chart(BTCHis.Close)
-
-#Ethereum
-st.write("ETHERUM ($)")
-imageETH = Image.open(urlopen('https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png'))
-#Display image
-st.image(imageETH)
-#Display dataframe
-st.table(ETH)
-#Display a chart
-st.bar_chart(ETHHis.Close)
-
-#Ripple
-st.write("RIPPLE ($)")
-imageXRP = Image.open(urlopen('https://s2.coinmarketcap.com/static/img/coins/64x64/52.png'))
-#Display image
-st.image(imageXRP)
-#Display dataframe
-st.table(XRP)
-#Display a chart
-st.bar_chart(XRPHis.Close)
-
-#Bitcoin Cash
-st.write("BITCOIN CASH ($)")
-imageBCH = Image.open(urlopen('https://s2.coinmarketcap.com/static/img/coins/64x64/1831.png'))
-#Display image
-st.image(imageBCH)
-#Display dataframe
-st.table(BCH)
-#Display a chart
-st.bar_chart(BCHHis.Close)
+for crypto_name, crypto_image, crypto_df, crypto_hist in cryptos:
+    st.write(f"{crypto_name} ($)")
+    st.image(crypto_image)
+    st.write("Historical Data:")
+    st.table(crypto_hist)
+    st.write("Data on Selected Date Range:")
+    st.table(crypto_df)
+    st.write("Price Chart:")
+    st.bar_chart(crypto_hist.Close)
